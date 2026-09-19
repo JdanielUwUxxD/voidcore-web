903f03cdf8baf02d11e256b46217ff2adf8bf22b
Darkyuu_ <153377149+JdanielUwUxxD@users.noreply.github.com>
2026-09-18 22:04:27 -0500

    Update Loader.js

--- components/Loader.js
+++ components/Loader.js
@@ -1,1 +1,8 @@
-
+export default function Loader() {
+  return (
+    <div className="loader-screen">
+      <div className="void-loader" />
+      <p className="loader-label">VOIDCORE STUDIOS</p>
+    </div>
+  );
+}
